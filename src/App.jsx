7bfe9ec55bb7ffcc5c969366b7f01f88ece54a6a
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-07-04 17:00:53 -0400

    1 file modified

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+const chats = ["Heyy","gg"]
+
+const App = () =>{
+
+  return(
+    <section>
+      <h1>Chat App</h1>
+    </section>
+  )
+}
+
+export default App
